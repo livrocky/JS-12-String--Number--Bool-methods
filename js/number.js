cb0353fba6911547397feea230542a5f151785e6
rokas <rokasra@gmail.com>
2022-03-08 15:31:16 +0200

    number clear

--- js/number.js
+++ js/number.js
@@ -15,11 +15,10 @@ const inputEl = document.getElementById("favourite");
 const pEl = document.createElement("p");
 pEl.textContent = +inputEl.value + 50;
 inputEl.after(pEl);
-document.body.append();
+// atsakyma atnaujinti kei keiciasi reikme ivesties lauke
 
 // pagaminti masyva is
 // <p id="nr">20, 50, 60, 0, -42, -2, 100</p> el reiksmiu
-
 const pStringEl = document.getElementById("nr");
 const pElStringValue = pStringEl.textContent;
 console.log("pElStringValue===", pElStringValue);
@@ -38,10 +37,21 @@ console.log("DoubleArr===", doubleArr);
 
 const pResultEl = document.createElement("p");
 pResultEl.textContent = doubleArr.join(", ");
+// pResultEl.textContent = { userName: 'james' };
 pStringEl.after(pResultEl);
 
 //// suapvalinti 8.5471 iki 2 skaiciu po kablelio
 
 const val = 8.5471;
 const apvalinta = val.toFixed(2);
-console.log("apvalinta===", apvalinta);
+console.log("apvalinta ===", apvalinta);
+console.log("apvalinta ===", typeof apvalinta);
+
+const mixedArray = [5, "blue", true, false, "Mike", 10, "50", 0, -1, "age"];
+// atrinkti i atskirus masyvus visus
+// string tipo duomenis
+// boolean tipo duomenis
+// skaiciu tipo duomenis
+
+// gauti skaiciu tipo duomenu vidurki
+// isitikinti kad suskaiciuota ir atrinkta teisingai
